c57a6135913d1c2e459751347ca9db5167cebd98
Kori238 <jakeedwards238@gmail.com>
2024-01-24 22:23:53 +0000

    Man speaks properly
    he has a longing for bricks i think

--- Assets/Interactable/Man.cs
+++ Assets/Interactable/Man.cs
@@ -8,25 +8,39 @@ public class Man : MonoBehaviour
 {
     [SerializeField] TextMeshPro textDisplay;
     public bool displayingText = false;
+    [SerializeField] float textDisplayLength = 5f;
+    float textDuration;
 
     void Start()
     {
         Globals.Instance.man = this;
+        textDisplay.enabled = false;
     }
 
     public void SetText(string text)
     {
         Debug.Log("ass");
+        if (!displayingText) {
         StartCoroutine(UpdateText(text));
+        StartCoroutine(HideTextDelay());
+        }
+    }
+
+    public IEnumerator HideTextDelay()
+    {
+        textDuration = textDisplayLength;
+        textDisplay.enabled = true;
+        while (textDuration > 0)
+        {
+            yield return new WaitForSeconds(0.1f);
+            textDuration -= 0.1f;
+        }
+        textDisplay.enabled = false;
     }
 
     public IEnumerator UpdateText(string newText)
     {
         Debug.Log("oui");
-        if (displayingText)
-        {
-            yield break;
-        }
         displayingText = true;
         textDisplay.text = "";
         foreach (char c in newText)
